46737c552d91d0fe437dfcbf35d596f60e971e7d
Svene <cabreraaidre@gmail.com>
2022-10-22 19:37:43 +0800

    Genesis of Lab Activities for First Year in University of Mindanao (CCE109 - Fundamentals of Computing) by Aidre Cabrera

--- src/firstLab/Main.java
+++ src/firstLab/Main.java
@@ -4,24 +4,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+        int userSession = 1;
+        do {
+            Scanner userInput = new Scanner(System.in);
 
-        System.out.print("Enter First Name: ");
-        String userFN = userInput.nextLine();
+            System.out.print("\n");
+            System.out.println("-----------------------------------------");
+            System.out.println("          University of Mindanao         ");
+            System.out.println("           BS Computer Science           ");
+            System.out.println("             Lab Assignment 1            ");
+            System.out.println("             by Aidre Cabrera            ");
+            System.out.println("             Session: " + userSession + "           ");
+            System.out.println("-----------------------------------------");
+            System.out.print("\n");
 
-        System.out.print("Enter Middle Name: ");
-        String userMN = userInput.nextLine();
+            System.out.println("                 Input                  ");
+            System.out.println("-----------------------------------------");
+            System.out.print("Enter First Name: ");
+            String userFN = userInput.nextLine();
+            System.out.print("Enter Middle Name: ");
+            String userMN = userInput.nextLine();
+            System.out.print("Enter Last Name: ");
+            String userLN = userInput.nextLine();
+            System.out.print("Enter ID Number: ");
+            int userID = userInput.nextInt();
+            System.out.println("-----------------------------------------");
+            System.out.print("\n");
 
-        System.out.print("Enter Last Name: ");
-        String userLN = userInput.nextLine();
+            String userMI = userMN.charAt(0) + ".";
 
-        System.out.print("Enter ID Number: ");
-        int userID = userInput.nextInt();
+            System.out.println("                 Output                  ");
+            System.out.println("-----------------------------------------");
+            System.out.println("Full Name: " + userFN + " " + userMI + " " + userLN);
+            System.out.println("ID Number: " + userID);
+            System.out.println("-----------------------------------------");
+            System.out.print("\n");
 
-        String userMI = String.valueOf(userMN.charAt(0)) + ".";
+            System.out.println("Want to try again?");
+            System.out.println("Type 1 for Yes");
+            System.out.println("Type 0 for No");
+            System.out.print("\n");
 
-        System.out.println("Full Name: " + userFN + " " + userMI + " " + userLN);
-        System.out.println("ID Number: " + userID);
+            System.out.print("Kindly enter here: ");
+            userSession = userInput.nextInt();
+
+        } while (userSession == 1);
+
+        System.out.print("\n");
+        System.out.println("Thank you for using my program! :)");
     }
 
 }
