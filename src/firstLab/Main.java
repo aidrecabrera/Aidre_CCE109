6910ec793d585fe7881042299fca31ed3dad4d82
Svene <cabreraaidre@gmail.com>
2022-10-22 19:20:31 +0800

    Genesis of Lab Activities for First Year in University of Mindanao (CCE109 - Fundamentals of Computing) by Aidre Cabrera

--- src/firstLab/Main.java
+++ src/firstLab/Main.java
@@ -1,4 +1,27 @@
 package firstLab;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String userFN = userInput.nextLine();
+
+        System.out.print("Enter Middle Name: ");
+        String userMN = userInput.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String userLN = userInput.nextLine();
+
+        System.out.print("Enter ID Number: ");
+        int userID = userInput.nextInt();
+
+        String userMI = String.valueOf(userMN.charAt(0)) + ".";
+
+        System.out.println("Full Name: " + userFN + " " + userMI + " " + userLN);
+        System.out.println("ID Number: " + userID);
+    }
+
 }
